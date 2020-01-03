9f46f9371c50fc3a642049345c1afcd04d532054
AngryPhilomel <an.aries@icloud.com>
2020-01-03 11:01:07 +0300

    v.0.3

--- src/components/show-day/show-day.js
+++ src/components/show-day/show-day.js
@@ -1,15 +1,65 @@
 import React, { Component } from 'react';
+import ApiService from "../../services/api-service";
 
+
+const apiService = new ApiService();
 
 export default class ShowDay extends Component {
+
+    // state = {
+    //     event: null
+    // }
+
+    componentDidMount() {
+        const {year, month, date} = this.props;
+        if (year > 0) {
+            apiService.getDate(+year, +month + 1, +date)
+                .then((event) => {
+                    try {
+                        event.forEach((e) => {
+                            console.log(e.title)
+                        })
+
+                        } catch (e) {
+                        // console.log('пусто')
+                        }
+
+                });
+        }
+    }
+            // const res = apiService.getDate(+year, +month +1, +date)
+            //     console.log(res)
+
+
+
+
+    // getDate = (year, month, date) => {
+    //     if (year > 0) {
+    //         const res = apiService.getDate(year, month, date);
+    //         this.setState({
+    //             event: res
+    //         });
+    //         return res
+    //     }
+
+    // };
+
+
     render() {
         const {date, month, year} = this.props;
 
+        // if (year > 0) {
+        //     const res = this.getDate(+year, +month+1, +date);
+        //     console.log(res)
+        // }
+
+
         return (
             <td
-                onClick={()=>alert(`${date}-${month+1}-${year}`)}
+                onClick={()=>alert(`${+date}-${+month+1}-${+year}`)}
             >
                 {date}
+
             </td>
         );
     }
--- src/services/api-service.js
+++ src/services/api-service.js
@@ -2,37 +2,37 @@ export default class ApiService {
 
     _apiBase = 'http://18.220.208.49/api';
 
+
     getResourse = async (url) => {
         const res = await fetch(`${this._apiBase}${url}`);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url}` +
-                `, received ${res.status}`)
+        if (res.ok) {
+            return await res.json();
         }
-        return res.json();
     };
 
     getDate = async (year, month, date) => {
-        const res = await this.getResourse(`/${year}/${month}/${date}/?format=json`);
-        console.log(res)
-
-        // console.log(this._transformEvent(res))
+        const res = await this.getResourse(`/${year}/${month}/${date}/`);
+        if (res !== undefined) {
+            // console.log(res);
+            return res;
+        }
 
     };
-
-    _transformEvent = (event) => {
-        return {
-            id: event.id,
-            category: event.category,
-            description: event.description,
-            period: event.period,
-            purpose: event.purpose,
-            title: event.title,
-            to: event.to,
-            type: event.type,
-            who: event.who,
-        }
-    }
-
-
 }
+
+
+    // _transformEvent = (event) => {
+    //     return {
+    //         id: event.id,
+    //         category: event.category,
+    //         description: event.description,
+    //         period: event.period,
+    //         purpose: event.purpose,
+    //         title: event.title,
+    //         to: event.to,
+    //         type: event.type,
+    //         who: event.who,
+    //     };
+    // };
+
+
